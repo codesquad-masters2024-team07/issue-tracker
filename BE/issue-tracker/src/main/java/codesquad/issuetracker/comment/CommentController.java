f31f6dee879ad3adb9e5a15326362c0200b16edb
Keon Kim <fnc6488@gmail.com>
2024-05-30 14:10:55 +0900

    :sparkles: feat: 댓글 내용 수정 기능 구현

---
+++ BE/issue-tracker/src/main/java/codesquad/issuetracker/comment/CommentController.java
@@ -0,0 +1,25 @@
+package codesquad.issuetracker.comment;
+
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/comments")
+public class CommentController {
+
+    private final CommentService commentService;
+
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @PatchMapping("/{commentId}")
+    public void updateContent(@PathVariable Long commentId, @RequestBody CommentUpdateRequest request) {
+        commentService.updateContent(commentId, request);
+    }
+
+}
--- BE/issue-tracker/src/main/java/codesquad/issuetracker/comment/CommentResponse.java
+++ BE/issue-tracker/src/main/java/codesquad/issuetracker/comment/CommentResponse.java
@@ -6,13 +6,14 @@ import lombok.Value;
 @Value
 public class CommentResponse {
 
+    Long id;
     String authorId;
     String contents;
     LocalDateTime createdAt;
     LocalDateTime updatedAt;
 
     public static CommentResponse of(Comment comment) {
-        return new CommentResponse(comment.getAuthorId().getId(), comment.getContents(),
+        return new CommentResponse(comment.getId(), comment.getAuthorId().getId(), comment.getContents(),
             comment.getCreatedAt(), comment.getUpdatedAt());
     }
 
